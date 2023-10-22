53932f0c455c747ce1371fbd7b55e0efa35882de
NurlanKerimli <145684028+NurlanKerimli@users.noreply.github.com>
2023-10-22 20:11:53 +0400

    BankConsoleApplication Created

---
+++ BankConsoleApplication/BankConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
